88fb4685981416d4b828fbba09c61f5e34215574
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:29:39 +0300

    Add files via upload

---
+++ main (17).php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// PHP FONKSIYONLARA GIRIS
+
+function azalt(&$sayi) {
+    $sayi = $sayi+10;
+    
+}
+$degisken = 10;
+azalt($degisken);
+echo($degisken);
+//  bu bölümdede phpde referansla fonksiyon kullanımını gördük
+
+
+
+
+
+?>
